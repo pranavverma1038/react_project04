483a838d423584481de1a71ff410a71805f7db92
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-28 04:49:51 +0530

    adding result response and loading on home and adding its css

--- react_project_04/src/context/Context.jsx
+++ react_project_04/src/context/Context.jsx
@@ -1,21 +1,48 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import generateResponse from "../config/gemini";  // Correct import
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
   
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPreviousPrompts] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
+
+
+
+
   const onSent = async (prompt) => {
-    const response = await generateResponse(prompt); 
-    console.log(response);   
+    setResultData("")
+    setLoading(true)
+    setShowResult(true)
+    setRecentPrompt(input)
+    const response = await generateResponse(input); 
+    setResultData(response)
+    setLoading(false)
+    setInput("")
+
+       
   };
 
   // Test call
-  onSent("what is react js");
+  
 
   const contextValue = {
-    
-  };
+     prevPrompts,
+     setPreviousPrompts,
+     onSent,
+     setRecentPrompt,
+     recentPrompt,
+     showResult,
+     loading,
+     resultData,
+     input,
+     setInput,
+  }
 
   return (
     <Context.Provider value={contextValue}>
