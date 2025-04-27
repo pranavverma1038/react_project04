8e3f64233c42acc9febb7894908a0f0046239fcc
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-28 03:53:31 +0530

    added new gemini api key response

--- react_project_04/src/config/gemini.js
+++ react_project_04/src/config/gemini.js
@@ -1,15 +1,17 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 
 const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
-const ai = new GoogleGenerativeAI(API_KEY);
+const ai = new GoogleGenAI({apiKey: API_KEY});
 console.log(ai);
 async function generateResponse(prompt) {
-  const model = ai.getGenerativeModel({ model: "gemini-2.0-flash" });
-
-  const result = await model.generateContent(prompt);
-  const response =  result.response;
-  const text = response.text();
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: prompt,
+      });
+  
+  const text = response.text;
+  console.log(text)
 
   return text;
 }
