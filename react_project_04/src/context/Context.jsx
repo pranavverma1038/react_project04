18c39be8d6d7d12821a57140b2502a5bd2542bc8
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-28 20:38:39 +0530

    adding optimized text response

--- react_project_04/src/context/Context.jsx
+++ react_project_04/src/context/Context.jsx
@@ -12,7 +12,11 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
 
-
+    const delayPara = (index,nextWord) => {
+        setTimeout(()=>{
+            setResultData(prev=>prev+nextWord)
+        },75*index)
+    }
 
 
   const onSent = async (prompt) => {
@@ -20,8 +24,23 @@ const ContextProvider = (props) => {
     setLoading(true)
     setShowResult(true)
     setRecentPrompt(input)
-    const response = await generateResponse(input); 
-    setResultData(response)
+    const response = await generateResponse(input);
+    let responseArray = response.split("**")
+    let newResponse
+    for(let i = 0 ; i<responseArray.length; i++){
+      if(i === 0 || i%2!==1){
+        newResponse += responseArray[i];
+      }else{
+        newResponse += "<b>"+responseArray[i]+"</b>"
+      }
+    }
+
+    let lineBreak = newResponse.split("*").join("<br>")
+    let newResponseArray= lineBreak.split(" ");
+    for(let i=0; i< newResponseArray.length; i++){
+      const nextWord=newResponseArray[i];
+      delayPara(i,nextWord+" ");
+    }
     setLoading(false)
     setInput("")
 
